c244b6bf561dafbb0bb6b1cb4a5a9e7e176a5906
Murat Genc <muratgenc@Murats-MacBook-Pro.local>
2026-03-23 15:56:25 -0500

    Remove basePath for custom domain

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -34,7 +34,7 @@ export default function Navbar() {
           {/* Logo */}
           <a href="#" className="flex items-center gap-3 group">
             <Image
-              src="/visionbridge.ai/visionbridge-logo.png"
+              src="/visionbridge-logo.png"
               alt="VisionBridge.ai"
               width={44}
               height={44}
